51dffad1bc3a6b850ef9269b9bc3996173ffb9dc
pat-s <patrick.schratz@gmail.com>
2020-07-25 21:19:54 +0200

    next

--- tools/rpkg/tic.R
+++ tools/rpkg/tic.R
@@ -1,5 +1,9 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(error_on = "warning")
+if (Sys.info()[["sysname"]] != "Windows") {
+  do_package_checks(error_on = "warning", codecov = FALSE)
+} else {
+  do_package_checks(error_on = "warning", codecov = FALSE)
+}
 
 if (Sys.info()[["sysname"]] == "Windows") {
   get_stage("after_success") %>%
@@ -11,13 +15,13 @@ if (Sys.info()[["sysname"]] == "Windows") {
   get_stage("after_success") %>%
     add_code_step(tools::write_PACKAGES(type = "mac.binary")) %>%
     add_code_step(system2("python3", c("../upload-s3.py", "rstats", "duckdb_*.tgz"))) %>%
-    add_code_step(system2("python3", c("../upload-s3.py", "rstats/bin/macosx/contrib/4.0", "duckdb_*.tgz","PACKAGES*"))) %>%
+    add_code_step(system2("python3", c("../upload-s3.py", "rstats/bin/macosx/contrib/4.0", "duckdb_*.tgz", "PACKAGES*"))) %>%
     add_code_step(system2("python3", c("../../scripts/asset-upload.py", "duckdb_r_osx.tgz=duckdb_*.tgzz")))
 
 } else if (Sys.info()[["sysname"]] == "Linux") {
   get_stage("after_success") %>%
     add_code_step(tools::write_PACKAGES(type = "source")) %>%
     add_code_step(system2("python3", c("../upload-s3.py", "rstats", "duckdb_*.tar.gz"))) %>%
-    add_code_step(system2("python3", c("../upload-s3.py", "rstats/src/contrib", "duckdb_*.tar.gz","PACKAGES*"))) %>%
+    add_code_step(system2("python3", c("../upload-s3.py", "rstats/src/contrib", "duckdb_*.tar.gz", "PACKAGES*"))) %>%
     add_code_step(system2("python3", c("../../scripts/asset-upload.py", "duckdb_r_src.tar.gz=duckdb_*.tar.gz")))
 }
